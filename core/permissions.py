353a46b6e7e82251dc4e7974a19df81cf920d195
FranckMK <maniranyibutsef@gmail.com>
2025-11-14 11:02:51 +0200

    Resolution encore des conflicts

--- core/permissions.py
+++ core/permissions.py
@@ -28,6 +28,12 @@ class IsSuperAdmin(RolePermission):
 class IsRector(RolePermission):
     allowed_roles = ["rector"]
 
+class IsSuperAdminCreateOnly(IsSuperAdmin):
+    def has_permission(self, request, view):
+        if request.method == "POST":
+            return super().has_permission(request, view)
+        return True
+
 
 class IsDirectorAcademic(RolePermission):
     allowed_roles = ["director_academic"]
