6187e7563ed6b70081c8c1e39e5b17e48be0008f
unknown <christiamani55@gmail.com>
2025-11-27 15:28:32 +0200

    urls/fac, urls/ums, admin, serializers, tests, views

--- services/core_service/academic_module/faculty_app/tests.py
+++ services/core_service/academic_module/faculty_app/tests.py
@@ -1,1 +1,24 @@
-# Create your tests here.
+from django.test import TestCase
+
+from services.core_service.academic_module.university_app.models import University
+
+from .models import Faculty, TypeFormation
+
+
+class FacultyModelTest(TestCase):
+    def setUp(self):
+
+        self.university = University.objects.create(name="Test University")
+
+        self.type_formation = TypeFormation.objects.create(name="Licence", code="L")
+
+    def test_create_faculty(self):
+
+        faculty = Faculty.objects.create(
+            faculty_name="Science",
+            faculty_abreviation="SCI",
+            types=self.type_formation,
+            university=self.university,
+        )
+        self.assertEqual(str(faculty), "Science")
+        self.assertEqual(faculty.university.name, "Test University")
