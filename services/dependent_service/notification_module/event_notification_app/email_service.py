3e81010422030ef1359164e7eb3026043fbd7b6e
FranckMK <maniranyibutsef@gmail.com>
2025-12-01 12:08:48 +0200

    creation des fonctionnalite pour les services aux etudiants

--- services/dependent_service/notification_module/event_notification_app/email_service.py
+++ services/dependent_service/notification_module/event_notification_app/email_service.py
@@ -1,5 +1,5 @@
-from django.core.mail import send_mail
 from django.conf import settings
+from django.core.mail import send_mail
 
 
 def send_notification_email(email, message):
@@ -8,7 +8,7 @@ def send_notification_email(email, message):
     """
     try:
         send_mail(
-            subject='Notification UMS',
+            subject="Notification UMS",
             message=message,
             from_email=settings.EMAIL_HOST_USER,
             recipient_list=[email],
