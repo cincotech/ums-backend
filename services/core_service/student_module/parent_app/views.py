bfb01c819283585cd3c073cf95be3e085968e175
Joviale492 <jovialelorraine6@gmail.com>
2025-12-01 11:07:02 +0200

    parent

--- services/core_service/student_module/parent_app/views.py
+++ services/core_service/student_module/parent_app/views.py
@@ -1,16 +1,22 @@
 # Create your views here.
 
-from core.views import BaseViewSet
+from rest_framework import viewsets
+from rest_framework.filters import SearchFilter
+from rest_framework.permissions import IsAuthenticated
 
 from .models import Parent, Profession
 from .serializers import ParentSerializer, ProfessionSerializer
 
 
-class ProfessionViewSet(BaseViewSet):
+class ProfessionViewSet(viewsets.ModelViewSet):
     queryset = Profession.objects.all()
     serializer_class = ProfessionSerializer
+    permission_classes = [IsAuthenticated]
 
 
-class ParentViewSet(BaseViewSet):
+class ParentViewSet(viewsets.ModelViewSet):
     queryset = Parent.objects.all()
     serializer_class = ParentSerializer
+    permission_classes = [IsAuthenticated]
+    filter_backends = [SearchFilter]
+    search_fields = ["parent_name", "parent_phone", "parent_email"]
