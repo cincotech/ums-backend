c584ae35f485e5dc503cc2ee46e15f3220a5633b
Janeiro hurley <ndabubahajanvier@gmail.com>
2026-04-25 17:20:57 +0200

    Mettre à jour le sérialiseur University pour inclure le champ country_name et modifier les champs exposés

--- services/core_service/academic_module/university_app/serializers.py
+++ services/core_service/academic_module/university_app/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 
+from services.foundational_service.geo_module.country_app.models import Country
 from .models import AcademicYear, University, UniversityDegree
 
 
@@ -10,9 +11,11 @@ class AcademicYearSerializer(serializers.ModelSerializer):
 
 
 class UniversitySerializer(serializers.ModelSerializer):
+    country_name = serializers.CharField(source='country.country_name', read_only=True, allow_null=True)
+
     class Meta:
         model = University
-        fields = "__all__"
+        fields = ['id', 'university_name', 'university_abrev', 'country', 'country_name']
 
 
 class UniversityDegreeSerializer(serializers.ModelSerializer):
