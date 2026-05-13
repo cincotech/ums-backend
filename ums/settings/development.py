5d204edd1bd1e03aa8ae36e7cd40027f5cfd89fa
Janeiro hurley <ndabubahajanvier@gmail.com>
2026-05-13 16:40:36 +0200

    fix(development): corriger les options de connexion à la base de données pour utiliser utf8mb4

--- ums/settings/development.py
+++ ums/settings/development.py
@@ -13,9 +13,10 @@ DATABASES = {
         'PASSWORD': 'bujumbura',
         'HOST': '127.0.0.1',
         'PORT': '3306',
-        'OPTIONS': {
-            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
-        }
+        "OPTIONS": {
+            "charset": "utf8mb4",
+            "init_command": "SET NAMES utf8mb4 COLLATE utf8mb4_unicode_ci",
+        },
     }
 }
 
